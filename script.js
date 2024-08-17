c4d59a02054021aacca260a9c39da15caab6e564
Dev <83638291+deva077dk@users.noreply.github.com>
2024-08-17 18:29:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+
+
+
+
+
+  
